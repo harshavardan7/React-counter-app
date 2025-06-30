f296b37518b23117c58267250b0d4c98cd7f7602
harshavardan7 <17harshavardan@gmail.com>
2025-06-30 11:41:02 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Counter from './counter';
 
 function App() {
   const[darkMode,setDarkMode]=useState(false);
+  const[text,setText]=useState(false);
   return (
    <div className={darkMode?" container dark":"container"}>
     <h1>Click Counter App</h1>
@@ -11,6 +12,10 @@ function App() {
     <button className='btn' onClick={()=>setDarkMode(!darkMode)}>
       {darkMode?"Switch to light Mode":"Switch to Dark Mode"}
     </button>
+    <button className='btn' onClick={()=>setText(! text)}>
+      {text?"Hide":"show"}
+    </button>
+    {text&& <p>Hello World</p>}
    </div>
   );
 }
